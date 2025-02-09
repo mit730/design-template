eeabd8130ff1232d4e9ec2e377dab7636534f728
mitesh <rajakumar730766@gmail.com>
2025-02-09 23:52:03 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
 import EcommercePage from './component/Ecommerce/EcommercePage';
-import FolderStrructure from './component/FolderStructure/FolderStrructure';
-import File from './component/RenderList';
-import StepperParent from './component/StepperForm/StepperParent';
-import StepperParentNew from './component/StepperFormWithoutLib/StepperForm/StepperParentNew';
-// import Question from './component/Question';
-// import InfiniteScroll from './component/InfiniteScroll';
 
 
 function App() {
   return (
     <div className="App">
-    {/* <InfiniteScroll /> */}
-    {/* <Question /> */}
-    {/* <FolderStrructure /> */}
-    {/* <File /> */}
-    {/* <StepperParent/> */}
-    {/* <StepperParentNew/> */}
-
     <EcommercePage />
     </div>
   );
